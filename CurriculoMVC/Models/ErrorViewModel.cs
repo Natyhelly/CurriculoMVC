bbd5c010b069e1383b0e7e98c9fa25fa8510aec4
Nathyelly L. Abreu <na.lagunaabreu@hotmail.com>
2022-06-17 10:21:18 -0300

    Add project files.

---
+++ CurriculoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurriculoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
