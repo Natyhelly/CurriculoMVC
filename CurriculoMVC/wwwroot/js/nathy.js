f3504d74e32018fafd6b1ad494aac577bead8510
Natyhelly <na.lagunaabreu@hotmail.com>
2022-06-30 12:02:52 -0300

    Tela de confirmação para deletar.

--- CurriculoMVC/wwwroot/js/nathy.js
+++ CurriculoMVC/wwwroot/js/nathy.js
@@ -1,4 +1,4 @@
-//document.addEventListener('DOMContentLoaded', function () {
+ï»¿//document.addEventListener('DOMContentLoaded', function () {
 //    numeral.register('locales', 'pt-br');
 //});
 
@@ -33,7 +33,7 @@ function calcular(operador, valor1, valor2) {
     return resultado.toString().replace('.', ',');
 }
 
-function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab calculadora.
+function montarCalculo(valor, tipo) { // FunÃ§Ã£o para montar o cÃ¡lculo na tab calculadora.
     var visor = document.getElementById('visor').innerText;
     var primeiroNumero = document.getElementById('primeiroNumero').innerText;
     var segundoNumero = document.getElementById('segundoNumero').innerText;
@@ -79,7 +79,7 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
         }
         return;
     }
-    if (primeiroNumero == '') { // Adiciona o primeiro número se não for 0.
+    if (primeiroNumero == '') { // Adiciona o primeiro nÃºmero se nÃ£o for 0.
         if (tipo == 'numero' && valor != '0' && valor != ',') {
             document.getElementById('primeiroNumero').innerText = valor;
             document.getElementById('visor').innerText = valor;
@@ -93,18 +93,18 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
         }
         return;
     }
-    if (operador == '') { // Acrescenta outro número se o operador e o total estiver vazio.
+    if (operador == '') { // Acrescenta outro nÃºmero se o operador e o total estiver vazio.
         if (tipo == 'numero') {
-            if (total == '') { // Se não é pra reiniciar ainda...
-                if ((valor == ',' && primeiroNumero.indexOf(',') == -1) || valor != ',') { // ... e já tiver vírgula, não põe outra.
-                    
+            if (total == '') { // Se nÃ£o Ã© pra reiniciar ainda...
+                if ((valor == ',' && primeiroNumero.indexOf(',') == -1) || valor != ',') { // ... e jÃ¡ tiver vÃ­rgula, nÃ£o pÃµe outra.
+
                     if (primeiroNumero.length >= 14)
                         return;
                     document.getElementById('primeiroNumero').innerText += valor;
                     document.getElementById('visor').innerText += valor;
                 }
                 return;
-            } else { // Se pode reiniciar, substitui pelo novo número (reinicializando a conta).
+            } else { // Se pode reiniciar, substitui pelo novo nÃºmero (reinicializando a conta).
                 document.getElementById('total').innerText = '';
                 if (valor == ',') {
                     document.getElementById('primeiroNumero').innerText = 0 + valor;
@@ -124,17 +124,17 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
                 } else
                     document.getElementById('visor').innerText += valor;
                 return;
-            } 
+            }
         }
-    } else { // Se já tiver operador
+    } else { // Se jÃ¡ tiver operador
         if (tipo == 'numero') {
             if ((segundoNumero == '' && valor != ',') || segundoNumero != '') {
-                if (!(segundoNumero == '0' && valor == '0')) { // Não adiciona mais de 1 zero.
-                    if (segundoNumero == '0' && valor != ',') { // Substitui o zero por outro número na primeira casa do segundoNumero.
+                if (!(segundoNumero == '0' && valor == '0')) { // NÃ£o adiciona mais de 1 zero.
+                    if (segundoNumero == '0' && valor != ',') { // Substitui o zero por outro nÃºmero na primeira casa do segundoNumero.
                         document.getElementById('segundoNumero').innerText = valor;
                         document.getElementById('visor').innerText = valor;
                     } else {
-                        if ((valor == ',' && segundoNumero.indexOf(',') == -1) || valor != ',') { // se já tiver vírgula, não põe outra ou põe números.
+                        if ((valor == ',' && segundoNumero.indexOf(',') == -1) || valor != ',') { // se jÃ¡ tiver vÃ­rgula, nÃ£o pÃµe outra ou pÃµe nÃºmeros.
                             document.getElementById('segundoNumero').innerText += valor;
                             document.getElementById('visor').innerText += valor;
                         }
@@ -147,7 +147,7 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
             }
             return;
         }
-        if (segundoNumero == '') { // Se já tiver operador e vier outro operador, substitui o mesmo.
+        if (segundoNumero == '') { // Se jÃ¡ tiver operador e vier outro operador, substitui o mesmo.
             if (valor == '=') {
                 resultadoCalcular = calcular(operador, parseFloat(primeiroNumero.replace(',', '.')), parseFloat(primeiroNumero.replace(',', '.')));
                 document.getElementById('primeiroNumero').innerText = resultadoCalcular;
@@ -158,7 +158,7 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
                 document.getElementById('operador').innerText = valor;
                 document.getElementById('visor').innerText = visor.substring(0, visor.length - 1) + valor;
             }
-        } else { // Se já tiver o segundo número, mostra o resultado e limpa o segundo número.
+        } else { // Se jÃ¡ tiver o segundo nÃºmero, mostra o resultado e limpa o segundo nÃºmero.
             resultadoCalcular = calcular(operador, parseFloat(primeiroNumero.replace(',', '.')), parseFloat(segundoNumero.replace(',', '.')));
             document.getElementById('primeiroNumero').innerText = resultadoCalcular;
             document.getElementById('segundoNumero').innerText = '';
@@ -166,7 +166,7 @@ function montarCalculo(valor, tipo) { // Função para montar o cálculo na tab cal
                 document.getElementById('visor').innerText = resultadoCalcular;
                 document.getElementById('operador').innerText = '';
                 document.getElementById('total').innerText = resultadoCalcular;
-            } else { // Se não, mostra o resultado + o novo operador.
+            } else { // Se nÃ£o, mostra o resultado + o novo operador.
                 document.getElementById('visor').innerText = resultadoCalcular + valor;
                 document.getElementById('operador').innerText = valor;
             }
@@ -322,7 +322,7 @@ document.addEventListener("keyup", function pressionarTecla(tecla) {
 function jogoDaVelha(casa) {
     var proximoValor = document.querySelector('#proximoValor').value;
 
-    //busca conteúdo da casa
+    //busca conteÃºdo da casa
     var conteudoCasa = document.getElementById(casa).innerText;
     var casaBloqueada = document.getElementById(casa).getAttribute('disabled') == 'disabled';
 
@@ -347,7 +347,7 @@ function jogoDaVelha(casa) {
     var casa5 = document.getElementById('casa-5').innerText;
     var casa6 = document.getElementById('casa-6').innerText;
     var casa7 = document.getElementById('casa-7').innerText;
-    var casa8 = document.getElementById('casa-8').innerText; 
+    var casa8 = document.getElementById('casa-8').innerText;
     var casa9 = document.getElementById('casa-9').innerText;
 
     if (casa1 == 'X' && casa2 == 'X' && casa3 == 'X')
@@ -416,11 +416,11 @@ function validaCamposFormulario(nome, genero, spotify, cadastro) {
         alert('Preencha todos os campos para salvar');
         return false;
     }
-    var padraoData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    if (!padraoData.test(cadastro)) {
-        alert("Digite a data no formato dd/mm/aaaa");
-        return false;
-    }
+    //var padraoData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+    //if (!padraoData.test(cadastro)) {
+    //    alert("Digite a data no formato dd/mm/aaaa");
+    //    return false;
+    //}
     return true;
 }
 
@@ -471,7 +471,10 @@ function gravarDados() {
 }
 
 function deletarLinha(id) {
-    document.getElementById(id).remove();
+    if (confirm("Tem certeza que deseja deletar essa mÃºsica?") == true)
+        document.getElementById(id).remove();
+    else
+        return;
 }
 
 function editarLinha(id) {
